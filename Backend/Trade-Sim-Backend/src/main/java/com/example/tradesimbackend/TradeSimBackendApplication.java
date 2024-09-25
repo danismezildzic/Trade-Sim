196475efc48ce3aab073a3cf1701fe7350e51d81
KletschTobias <113858830+KletschTobias@users.noreply.github.com>
2024-09-25 15:37:45 +0200

    feat: initialized spring application

---
+++ Backend/Trade-Sim-Backend/src/main/java/com/example/tradesimbackend/TradeSimBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.tradesimbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeSimBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeSimBackendApplication.class, args);
+	}
+
+}
---
+++ Backend/Trade-Sim-Backend/src/test/java/com/example/tradesimbackend/TradeSimBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tradesimbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeSimBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
